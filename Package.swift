c55211b36ad725c0d19f194d5ffd6d9ffb308525
Richard Price <richard.price@cellpointdigital.com>
2021-08-18 16:20:13 +0200

    Release 0.0.5 - Certificate pinning with TrustKit enabled

--- Package.swift
+++ Package.swift
@@ -6,43 +6,58 @@ let package = Package(
     products: [
         .library(
             name: "ACQPaymentGateway",
-            targets: ["ACQPaymentGateway", "Core", "ACQCore", "ACQRemoteClient", "ACQCardPayment", "ACQApplePay", "ACQNetworkSecurity"]),
+            targets: [
+                "ACQPaymentGateway",
+                "Core",
+                "ACQCore",
+                "ACQRemoteClient",
+                "ACQCardPayment",
+                "ACQApplePay",
+                "ACQNetworkSecurity",
+                "TrustKit"
+            ]
+        )
     ],
     targets: [
         .binaryTarget(
             name: "ACQPaymentGateway",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQPaymentGateway.zip",
-            checksum: "59071712017d19436c1f6a3589bf704b460f73c2ce5d93c3b91ed6add6d01ace"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQPaymentGateway.zip",
+            checksum: "75d1f7d5587049e70b7ec7f1e34906bc873c12cf0303f024857d6fc92340dd98% "
         ),
         .binaryTarget(
             name: "Core",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/Core.zip",
-            checksum: "769b01d0c125cd6cf91b3aa44b358c12eb6730a7a1cb34aa58fda2c310a68b20"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/Core.zip",
+            checksum: "3659cc10437fcbb1447f84618efdea4e4a3af95ee780b6016dbbe820711c0331"
         ),
         .binaryTarget(
             name: "ACQCore",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQCore.zip",
-            checksum: "796c92dcd99c60d1927e55bf4835ee1cdb4f367506b3e06451e09c6241f9e8b8"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQCore.zip",
+            checksum: "86af087c2f11ba8d9b3be491f6564c64828f2aa51470a3213f7b53202ff53e3f"
         ),
         .binaryTarget(
             name: "ACQRemoteClient",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQRemoteClient.zip",
-            checksum: "ce9d8f858b21ce896976347482f08e92aa3245d960de994d2dd52cd1146b1681"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQRemoteClient.zip",
+            checksum: "03079bb0beb4530e1c1a90887fee3020b18eab63007e56b83f6a3189636aa0ab"
         ),
         .binaryTarget(
             name: "ACQCardPayment",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQCardPayment.zip",
-            checksum: "aa45c52b39672a029e32d994c48825ce131dc70bdcdd83c5983de79d3b3c4330"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQCardPayment.zip",
+            checksum: "65d9b3873220523b2ccc638622978798c76c00d2eb6daed6d5a68e0e2f1960d6"
         ),
         .binaryTarget(
             name: "ACQApplePay",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQApplePay.zip",
-            checksum: "24676ad14035854d315e7805afdb2a20a11bb2eb5df98ef2fcda83a6f323649e"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQApplePay.zip",
+            checksum: "be20faf155abed26b86c7aea2425e895d10b400c4edae703c14cdcff75402e79"
         ),
         .binaryTarget(
             name: "ACQNetworkSecurity",
-            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.4/ACQNetworkSecurity.zip",
-            checksum: "8610a2dafbc7d84385bccac97184d92f7b7d59b2c6a1daf1f62b51c8b632a95b"
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/ACQNetworkSecurity.zip",
+            checksum: "6fa6d08dbe209d936435149b3345879f576de9b2fae0c4e3a69a1b8b7880b5b1"
+        ),
+        .binaryTarget(
+            name: "TrustKit",
+            url: "https://github.com/AcquiredSupport/Acquired-iOS-MacOS/releases/download/0.0.5/TrustKit.zip",
+            checksum: "5a185420ebfc9514c39c9fc15af06c54f2abf26c64c49299c3da061ab56f5d1b"
         )
     ]
 )
